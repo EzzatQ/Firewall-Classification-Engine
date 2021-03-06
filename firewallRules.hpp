57a5405af12ca20bb3fd44409967603403b26321
Ezzat Qupty <ezzat.qupty12345@gmail.com>
2021-03-06 15:05:11 +0200

    Refactoring

--- firewallRules.hpp
+++ firewallRules.hpp
@@ -8,7 +8,7 @@
 #ifndef firewallRules_hpp
 #define firewallRules_hpp
 
-#include <iostream>
+
 #include <set>
 #include <map>
 #include <string>
@@ -20,28 +20,12 @@ class PacketRules {
 	string ip;
 	bool allowed;
 public:
-	PacketRules(string ip, bool allowed): ip(ip), allowed(allowed){}
-	
-	string getIP() const {
-		return ip;
-	}
-	
-	bool isAllowed() const{
-		return allowed;
-	}
-	
-	bool operator<(const PacketRules& p) const {
-		return this->ip < p.ip;
-	}
-	
-	bool operator>(const PacketRules& p) const {
-		return this->ip > p.ip;
-	}
-	
-	bool operator==(const PacketRules& p) const {
-		return this->ip == p.ip;
-	}
-	
+    PacketRules(string ip, bool allowed);
+    string getIP() const;
+    bool isAllowed() const;
+	bool operator<(const PacketRules& p) const;
+    bool operator>(const PacketRules& p) const;
+    bool operator==(const PacketRules& p) const;
 };
 
 typedef map<string, set<PacketRules>> FirewallMap;
@@ -51,38 +35,8 @@ class FirewallRules {
 	FirewallMap rules;
 public:
 	
-	void insert(string username, string ip, bool allowed){
-		PacketRules ipRule(ip, allowed);
-		FirewallMap::iterator user = rules.find(username);
-		if(user == rules.end()){ //username does not exist
-			set<PacketRules> userRule;
-			userRule.insert(ipRule);
-			rules.insert(MapPair(username, userRule));
-		} else {//username exists
-			set<PacketRules>::iterator packetRule = user->second.find(ipRule);
-			if(packetRule != user->second.end()){ // rule for ip has already been defined
-				cout << "Rule Redefinition: rule for user: " << username << " and ip: " << ip << " has already been defined" << endl;
-			} else { //rule wasn't definned
-				user->second.insert(ipRule);
-			}
-		}
-	}
-	
-	bool isAllowed(string username, string ip){
-		PacketRules ipRule(ip, false);
-		FirewallMap::iterator user = rules.find(username);
-		if(user == rules.end()){
-			return true;//if no rules for user, allow
-		} else {
-			set<PacketRules>::iterator packetRule = user->second.find(ipRule);
-			if(packetRule == user->second.end()){
-				return true; // no rule for specified ip
-			} else {
-				return packetRule->isAllowed();
-			}
-		}
-		
-	}
+    void insert(string username, string ip, bool allowed);
+    bool isAllowed(string username, string ip);
 };
 
 
